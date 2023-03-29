b2c4a55487daced466b30bef2731b7c4522b56bc
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-03-29 22:48:25 +0900

    Day11

---
+++ src/Ex4_12.java
@@ -0,0 +1,11 @@
+class Ex4_12 {
+	public static void main(String[] args) { 
+		int i= 5;  //초기화, 반복할 횟수
+
+	    while(i--!=0) {   //while은 조건식의 연산결과가 true인 동안 반복. 증감식 i--. 
+	/*  while(i!=0)	{		
+		   i--;             while문과 for문은 서로 변환이 가능. for(int i=4;i>=0;--i) {} */
+			System.out.println(i + " - I can do it.");
+		}
+	} 
+}
